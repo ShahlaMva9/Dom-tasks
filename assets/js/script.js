49552d05d2efee1d76f9263ff5b054c11fe39afd
Shahla Maharramova <78307910+ShahlaMva9@users.noreply.github.com>
2022-08-25 09:57:36 +0400

    PortfolioData

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,6 @@ function createUl(listItems, container) {
     navLi.appendChild(navA);
     navUl.appendChild(navLi);
     if (el.children && el.children.length) {
-      console.log("sd");
       //   el.children; // data
       //   navLi; //li
       createUl(el.children, navLi);
@@ -114,3 +113,88 @@ function createIntroCol(data) {
 }
 
 createIntroCol(data);
+
+const portfolioData = [
+  {
+    title: "Ipsum feugiat et dolor",
+    description:
+      "Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.",
+    image: "images/pic02.jpg",
+  },
+  {
+    title: "Sed etiam lorem nulla",
+    description:
+      "Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.",
+    image: "images/pic03.jpg",
+  },
+  {
+    title: "Consequat et tempus",
+    description:
+      "Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.",
+    image: "images/pic04.jpg",
+  },
+  {
+    title: "Blandit sed adipiscing",
+    description:
+      "Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.",
+    image: "images/pic05.jpg",
+  },
+  {
+    title: "Etiam nisl consequat",
+    description:
+      "Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.",
+    image: "images/pic06.jpg",
+  },
+  {
+    title: "Dolore nisl feugiat",
+    description:
+      "Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.",
+    image: "images/pic07.jpg",
+  },
+];
+const myPortRow = document.querySelector("#main section .row");
+
+function myPort() {
+  portfolioData.forEach((el) => {
+    const col = document.createElement("div");
+    col.classList.add("col-4");
+
+    const section = document.createElement("section");
+    section.setAttribute("class", "box");
+
+    const anchor = document.createElement("a");
+    anchor.setAttribute("href", "#");
+    anchor.setAttribute("class", "image featured");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", el.image);
+    img.setAttribute("alt", "");
+
+    const header = document.createElement("header");
+
+    const h3 = document.createElement("h3");
+    h3.textContent = el.title;
+
+    const p = document.createElement("p");
+    p.textContent = el.description;
+
+    const footer = document.createElement("footer");
+
+    const ul = document.createElement("ul");
+    ul.setAttribute("class", "actions");
+
+    const li = document.createElement("li");
+
+    myPortRow.appendChild(col);
+    col.appendChild(section);
+    section.appendChild(anchor);
+    section.appendChild(header);
+    section.appendChild(p);
+    section.appendChild(footer);
+    anchor.appendChild(img);
+    header.appendChild(h3);
+    footer.appendChild(ul);
+    ul.appendChild(li);
+  });
+}
+myPort();
